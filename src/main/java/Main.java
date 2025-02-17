a744629c6666bf18d10f2a160415c83cf2b53285
alvaro <alvaro@example.com>
2025-02-17 12:25:27 +0100

    Initial test setup

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static int[] arrayOfOddIntegers(int n){
+        int[] array = new int[5];
+
+        return array;
+    }
+}
