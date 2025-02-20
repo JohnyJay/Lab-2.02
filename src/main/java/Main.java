7759e4428104933cef5820fc29efcbb7d2fe505b
alvaro <alvaro@example.com>
2025-02-20 16:51:17 +0100

    TASK TWO - Java Keywords

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,81 @@
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // TASK ONE
         System.out.println(Arrays.toString(arrayOfOddIntegers(0)));
         System.out.println(Arrays.toString(arrayOfOddIntegers(-5)));
         System.out.println(Arrays.toString(arrayOfOddIntegers(20)));
         System.out.println(Arrays.toString(arrayOfOddIntegers(33)));
+
+        //TASK TWO
+        String sentence1 ="I wish we could return to the past.";
+        String sentence2 ="Can you break this wall?";
+        String sentence3 ="I finally found what I was looking for.";
+        String sentence4 ="Can I order a long drink?";
+        String sentence5 ="Which way is the highway?";
+        String sentence6 ="I like to go to the park";
+        boolean s1;
+        boolean s2;
+        boolean s3;
+        boolean s4;
+        boolean s5;
+        boolean s6;
+
+        s1=containsJavaKeyWord(sentence1);
+        s2=containsJavaKeyWord(sentence2);
+        s3=containsJavaKeyWord(sentence3);
+        s4=containsJavaKeyWord(sentence4);
+        s5=containsJavaKeyWord(sentence5);
+        s6=containsJavaKeyWord(sentence6);
+
+        System.out.println("\nThe method will flag the following sentences if they contain java keyowrds:\n");
+        System.out.println("The sentence: '"+sentence1+"' has been flagged as "+s1);
+        System.out.println("The sentence: '"+sentence2+"' has been flagged as "+s2);
+        System.out.println("The sentence: '"+sentence3+"' has been flagged as "+s3);
+        System.out.println("The sentence: '"+sentence4+"' has been flagged as "+s4);
+        System.out.println("The sentence: '"+sentence5+"' has been flagged as "+s5);
+        System.out.println("The sentence: '"+sentence6+"' has been flagged as "+s6);
+
+
     }
-    
+
+    public static boolean containsJavaKeyWord(String sentence1) {
+        String filePath = "keywords.txt";
+        String line;
+        List<String> keywords = new ArrayList<>();
+        boolean flag = false;
+        try{
+            File file = new File(filePath);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                line = scanner.nextLine();
+                line.trim();
+                keywords.add(line);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        //System.out.println(keywords);
+
+
+        String[] values = sentence1.split(" ");
+
+        for(String value: values){
+            if(keywords.contains(value)){
+                //if we find the value in the keywords... flag!!!
+                flag = true;
+            }
+        }
+
+        return flag;
+
+    }
+
     public static int[] arrayOfOddIntegers(int n){
         int[] array = {0};
         int length = 0;
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
@@ -13,6 +14,13 @@ class MainTest {
     private int number2;
     private int number3;
     private int number4;
+    String sentence1;
+    String sentence2;
+    String sentence3;
+    String sentence4;
+    String sentence5;
+    String sentence6;
+
 
     @BeforeEach
     void setUp(){
@@ -24,11 +32,16 @@ class MainTest {
         list3 = new int[]{0};
         number4 = 33;
         list4 = new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33};
-
+        sentence1 ="I wish we could return to the past.";
+        sentence2 ="Can you break this wall?";
+        sentence3 ="I finally found what I was looking for.";
+        sentence4 ="Can I order a long drink?";
+        sentence5 ="Which way is the highway?";
+        sentence6 ="I like to go to the park";
     }
 
     @org.junit.jupiter.api.Test
-    void testarrayOfOddIntegers() {
+    void testArrayOfOddIntegers_differentInts_correctList() {
         //assertEquals(list1,Main.arrayOfOddIntegers(number1));
         //assertTrue(list1.equals(Main.arrayOfOddIntegers(number1)));
         assertEquals(Arrays.toString(list1),Arrays.toString(Main.arrayOfOddIntegers(number1)));
@@ -38,7 +51,20 @@ class MainTest {
         assertEquals(Arrays.toString(list3),Arrays.toString(Main.arrayOfOddIntegers(number3)));
         //assertTrue(list4.equals(Main.arrayOfOddIntegers(number4)));
         assertEquals(Arrays.toString(list4),Arrays.toString(Main.arrayOfOddIntegers(number4)));
-
-
     }
+
+    @Test
+    void containsJavaKeyWord_stringSentences_result() {
+        assertTrue(Main.containsJavaKeyWord(sentence1));
+        assertTrue(Main.containsJavaKeyWord(sentence2));
+        assertTrue(Main.containsJavaKeyWord(sentence3));
+        assertTrue(Main.containsJavaKeyWord(sentence4));
+        assertFalse(Main.containsJavaKeyWord(sentence5));
+        assertFalse(Main.containsJavaKeyWord(sentence6));
+    }
+
+    
+
+
+
 }
